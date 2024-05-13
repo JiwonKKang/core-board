e191a5fabbad7e687eb055cb88b7bfd136c6a2b4
강지원 <kangji149@naver.com>
2024-05-13 19:00:59 +0900

    init

---
+++ src/main/java/com/core/corebackboard/CoreBackBoardApplication.java
@@ -0,0 +1,12 @@
+package com.core.corebackboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  CoreBackBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBackBoardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/core/corebackboard/CoreBackBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.corebackboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBackBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
